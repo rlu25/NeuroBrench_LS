3309f6cfd5a068ba9ca2fcb8091f5ffabc5b2965
rlu25 <44712844+rlu25@users.noreply.github.com>
2025-05-19 17:53:03 -0400

    Update test_RL.py

--- Benchmark_Finetune/Subcortical Segmentation/quickNAT_MAE/test_RL.py
+++ Benchmark_Finetune/Subcortical Segmentation/quickNAT_MAE/test_RL.py
@@ -2,5 +2,3 @@ import os
 import glob
 import nibabel as nib
 import torch
-quicknat_model = torch.load("/opt/localdata/data/usr-envs/ruiying/Code/NeurIPS/QuickNet/quickNAT_MAE/saved_models/mae_pretrian/model_final.pth")
-print(quicknat_model)
